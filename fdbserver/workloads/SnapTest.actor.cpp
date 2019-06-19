784f6726f561c4b13de824d3fe810bb2e9b009ad
sramamoorthy <sramamoorthy@snowflake.net>
2019-06-19 11:23:08 -0700

    snap v2: test files changes

--- fdbserver/workloads/SnapTest.actor.cpp
+++ fdbserver/workloads/SnapTest.actor.cpp
@@ -82,6 +82,7 @@ public: // variables
 	std::string restartInfoLocation; // file location to store the snap restore info
 	int maxRetryCntToRetrieveMessage; // number of retires to do trackLatest
 	bool skipCheck; // disable check if the exec fails
+	int snapVersion; // snapVersion to invoke
 
 public: // ctor & dtor
 	SnapTestWorkload(WorkloadContext const& wcx)
@@ -97,6 +98,7 @@ public: // ctor & dtor
 		    getOption(options, LiteralStringRef("restartInfoLocation"), LiteralStringRef("simfdb/restartInfo.ini"))
 		        .toString();
 		skipCheck = false;
+		snapVersion = getOption(options, LiteralStringRef("version"), 1);
 	}
 
 public: // workload functions
@@ -231,14 +233,20 @@ public: // workload functions
 				self->snapUID = deterministicRandom()->randomUniqueID();
 				try {
 					StringRef snapCmdRef = LiteralStringRef("/bin/snap_create.sh");
-					Future<Void> status = snapCreate(cx, snapCmdRef, self->snapUID);
+					Future<Void> status = snapCreate(cx, snapCmdRef, self->snapUID, self->snapVersion);
 					wait(status);
 					break;
 				} catch (Error& e) {
+					++retry;
 					if (e.code() == error_code_cluster_not_fully_recovered ||
 						e.code() == error_code_txn_exec_log_anti_quorum) {
-						++retry;
 						if (retry > 3) {
+							snapFailed = true;
+							break;
+						}
+					}
+					if (self->snapVersion == 2) {
+						if (retry > 5) {
 							snapFailed = true;
 							break;
 						}
@@ -251,6 +259,7 @@ public: // workload functions
 			std::string uidStr = self->snapUID.toString();
 			ini.SetValue("RESTORE", "RestoreSnapUID", uidStr.c_str());
 			ini.SetValue("RESTORE", "BackupFailed", format("%d", snapFailed).c_str());
+			ini.SetValue("RESTORE", "BackupVersion", format("%d", self->snapVersion).c_str());
 			ini.SaveFile(self->restartInfoLocation.c_str());
 			// write the snapUID to a file
 			TraceEvent("SnapshotCreateStatus").detail("Status", !snapFailed ? "Success" : "Failure");
@@ -342,7 +351,7 @@ public: // workload functions
 				self->snapUID = deterministicRandom()->randomUniqueID();
 				try {
 					StringRef snapCmdRef = LiteralStringRef("/bin/snap_create1.sh");
-					Future<Void> status = snapCreate(cx, snapCmdRef, self->snapUID);
+					Future<Void> status = snapCreate(cx, snapCmdRef, self->snapUID, self->snapVersion);
 					wait(status);
 					break;
 				} catch (Error& e) {
